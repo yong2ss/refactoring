1f04a0459fa619d1f9085d1c32d40ce54111c502
yong2ss <korea92co.gmail.com>
2023-01-25 20:37:18 +0900

    feat: 함수 선언 변경하기

--- src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_01_change_method_declaration/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_01_change_method_declaration/StudyDashboard.java
@@ -11,13 +11,31 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/*
+    함수 선언 변경하기
+        - 메소드 이름 변경
+            - 주석을 먼저 작성해보고 그 내용에 맞는 메소드명으로 변경
+            - 좋은 이름을 가진 함수는 어떻게 구현되었는지 코드를 보지 않아도 이름만 보고도 이해 가능
+        - 매개변수 추가
+            - 매개변수는 함수 내부의 문맥을 결정, 의존성을 결정
+        - 매개변수 제거
+        - 시그니처 변경
+ */
 public class StudyDashboard {
 
     private Set<String> usernames = new HashSet<>();
 
     private Set<String> reviews = new HashSet<>();
 
-    private void studyReviews(GHIssue issue) throws IOException {
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어온다.
+     * @throws IOException
+     */
+    private void loadReviews() throws IOException {
+        GitHub gitHub = GithubConfig.getInstance();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
         List<GHIssueComment> comments = issue.getComments();
         for (GHIssueComment comment : comments) {
             usernames.add(comment.getUserName());
@@ -34,12 +52,8 @@ public class StudyDashboard {
     }
 
     public static void main(String[] args) throws IOException {
-        GitHub gitHub = GithubConfig.getInstance();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
         StudyDashboard studyDashboard = new StudyDashboard();
-        studyDashboard.studyReviews(issue);
+        studyDashboard.loadReviews();
         studyDashboard.getUsernames().forEach(System.out::println);
         studyDashboard.getReviews().forEach(System.out::println);
     }
