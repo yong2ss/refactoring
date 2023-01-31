5310812f9c5f7f9090d4e16adab4504d90646f3b
yong2ss <korea92co.gmail.com>
2023-01-31 20:16:41 +0900

    리팩토링 14 매개변수를 질의 함수로 바꾸기

--- src/main/java/me/whiteship/refactoring/_04_long_parameter_list/after/_14_replace_parameter_with_query/Order.java
+++ src/main/java/me/whiteship/refactoring/_04_long_parameter_list/after/_14_replace_parameter_with_query/Order.java
@@ -1,5 +1,12 @@
 package me.whiteship.refactoring._04_long_parameter_list.after._14_replace_parameter_with_query;
 
+/**
+ * 매개변수를 질의 함수로 바꾸기
+ * - 함수의 매개변수 목록은 함수의 다양성을 대변하며, 짧을수록 이해하기 좋다
+ * - 어떤 한 매개변수를 다른 매개변수를 통해 알아낼수 있다면 "중복 매개변수"일 수 있다
+ * - 매개변수에 값을 전달하는 것은 "함수를 호출하는 쪽"의 책임이며 호출하는 쪽의 책임을 줄이고 내부에서 되도록 처리하자.
+ * - "임시 변수를 질의 함수로 바꾸기" + "함수 선언 변경하기"
+ */
 public class Order {
 
     private int quantity;
@@ -13,11 +20,14 @@ public class Order {
 
     public double finalPrice() {
         double basePrice = this.quantity * this.itemPrice;
-        int discountLevel = this.quantity > 100 ? 2 : 1;
-        return this.discountedPrice(basePrice, discountLevel);
+        return this.discountedPrice(basePrice);
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    private int discountLevel() {
+        return this.quantity > 100 ? 2 : 1;
+    }
+
+    private double discountedPrice(double basePrice) {
+        return discountLevel() == 2 ? basePrice * 0.90 : basePrice * 0.95;
     }
 }
